8963048c625175601623bcb87b6bd2e4ff009fcf
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2025-10-21 20:37:34 -0300

    feat: Crear entidades (SesionRealizada, RecordPersonal, EventoCalendario)

---
+++ src/MicroservicioAsignacionCalendario.Domain/Entities/RecordPersonal.cs
@@ -0,0 +1,13 @@
+﻿namespace MicroservicioAsignacionCalendario.Domain.Entities
+{
+    public class RecordPersonal
+    {
+        Guid Id { get; set; }
+        Guid IdAlumno { get; set; }
+        Guid IdEjercicio { get; set; }
+        decimal PesoMax { get; set; }
+        int Series { get; set; }
+        int Repeticiones { get; set; }
+        DateTimeOffset FechaRegistro { get; set; }
+    }
+}
