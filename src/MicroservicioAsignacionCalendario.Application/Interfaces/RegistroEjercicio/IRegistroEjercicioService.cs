2c9d77a7e7afc9ff41244c01a8910951bb7112ae
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2025-10-24 15:09:18 -0300

    feat: agregar interfaces de services(AlumnoPlan, Calendario, RecordPersonal, RegistroEjercicio)

---
+++ src/MicroservicioAsignacionCalendario.Application/Interfaces/RegistroEjercicio/IRegistroEjercicioService.cs
@@ -0,0 +1,8 @@
+﻿namespace MicroservicioAsignacionCalendario.Application.Interfaces.RegistroEjercicio
+{
+    public interface IRegistroEjercicioService
+    {
+        Task<object> RegistrarEjercicioAsync(object registro);
+        Task<IEnumerable<object>> ObtenerRegistrosAsync(Guid alumno_id);
+    }
+}
