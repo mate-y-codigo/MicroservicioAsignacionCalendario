4dd6a0dc93bdf1344c501128beffa07b798eb8b5
lauta <lautaromoreira722@gmail.com>
2025-10-23 21:57:43 -0300

    feat: Se agregaron Response DTOs para microservicio de rutinas y ejercicios

---
+++ src/MicroservicioAsignacionCalendario.Application/DTOs/EjercicioSesion/EjercicioSesionResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroservicioAsignacionCalendario.Application.DTOs.EjercicioSesion
+{
+    public class EjercicioSesionResponse
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        public int SeriesObjetivo { get; set; }
+        [Required]
+        public int RepeticionesObjetivo { get; set; }
+        [Required]
+        public int PesoObjetivo { get; set; }
+        [Required]
+        public int Descanso { get; set; }
+        [Required]
+        public int Orden { get; set; }
+        [Required]
+        public Ejercicio Ejercicio { get; set; }
+    }
+}
---
+++ src/MicroservicioAsignacionCalendario.Application/DTOs/SesionEntrenamiento/SesionEntrenamientoResponse.cs
@@ -0,0 +1,24 @@
+﻿using MicroservicioAsignacionCalendario.Application.DTOs.EjercicioSesion;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroservicioAsignacionCalendario.Application.DTOs.SesionEntrenamiento
+{
+    public class SesionEntrenamientoResponse
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        public Guid IdPlanEntrenamiento { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public int Orden { get; set; }
+        [Required]
+        public List<EjercicioSesionShortResponse> EjerciciosSesion { get; set; }
+    }
+}
